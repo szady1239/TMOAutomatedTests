ef8434cc9d9f2c8d95c7d3d9f6236b15826cc792
PKOWALCZYK <pawel.kowalczyk@execon.pl>
2018-04-05 18:58:24 +0200

    Added second Test - FullOrderTest without phone

--- src/pl/execon/tmo/main/java/data/OfferData.java
+++ src/pl/execon/tmo/main/java/data/OfferData.java
@@ -1,5 +1,17 @@
 package pl.execon.tmo.main.java.data;
 
 public class OfferData {
-    
+    private String offerPrice;
+
+    public OfferData(String offerPrice) {
+        this.offerPrice = offerPrice;
+    }
+
+    public String getOfferPrice() {
+        return offerPrice;
+    }
+
+    public void setOfferPrice(String offerPrice) {
+        this.offerPrice = offerPrice;
+    }
 }
